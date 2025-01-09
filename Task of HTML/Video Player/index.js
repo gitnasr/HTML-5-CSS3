d1e288a3dcdd7f3d7a9c35900053309a52925793
Mahmoud Nasr <239.nasr@gmail.com>
2025-01-09 19:15:37 +0200

    Video Player

---
+++ Task of HTML/Video Player/index.js
@@ -0,0 +1,48 @@
+const video = document.getElementById("video-player");
+
+const playButton = document.getElementById("play");
+const stopButton = document.getElementById("stop");
+const progress = document.getElementById("seek-bar");
+
+const toEndBtn = document.getElementById("toEnd");
+const toStartBtn = document.getElementById("toStart");
+const seek5Btn = document.getElementById("seek5");
+const seekminus5Btn = document.getElementById("seekminus5");
+const SeekBar = document.getElementById("seek-bar");
+function stopVideo() {
+	video.pause();
+}
+
+function playVideo() {
+	if (video.paused) {
+		video.play();
+	}
+}
+
+function seek5() {
+	video.currentTime += 5;
+}
+function seekminus5() {
+	video.currentTime -= 5;
+}
+
+function toEnd() {
+	video.currentTime = video.duration;
+}
+function toStart() {
+	video.currentTime = 0;
+}
+
+playButton.addEventListener("click", playVideo);
+stopButton.addEventListener("click", stopVideo);
+progress.addEventListener("change", () => {
+	video.currentTime = progress.value;
+});
+
+toEndBtn.addEventListener("click", toEnd);
+toStartBtn.addEventListener("click", toStart);
+seek5Btn.addEventListener("click", seek5);
+seekminus5Btn.addEventListener("click", seekminus5);
+video.addEventListener("timeupdate", () => {
+	SeekBar.value = video.currentTime;
+});
